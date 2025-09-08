ed71a6c49834ade2995b2fbb50d058825a8a69d2
AhmedSabrye <ahmedsabry.fr@gmail.com>
2025-09-08 09:33:15 +0300

    Comment out DonateCard component in Typing page, will be added in the future.

--- src/pages/Typing/index.tsx
+++ src/pages/Typing/index.tsx
@@ -131,7 +131,7 @@ const App: React.FC = () => {
   return (
     <TypingContext.Provider value={{ state: state, dispatch }}>
       <EnhancedPromotionModal />
-      {state.isFinished && <DonateCard />}
+      {/* {state.isFinished && <DonateCard />} */}
       {state.isFinished && <ResultScreen />}
       <Layout>
         <Header>
